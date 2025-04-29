3b01bc669f52550e55f83d12cf6100c5f8f9e48d
n-ckk <nickngqh@gmail.com>
2025-04-29 19:41:51 +0800

    logo 2.0

--- src/Staff.java
+++
@@ -1,8 +0,0 @@
-abstract class Staff extends User {
-    protected String role;
-
-    public Staff(String username, String password, String role) {
-        super(username, password);
-        this.role = role;
-    }
-}
